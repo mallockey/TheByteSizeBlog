0d4afb36babba6c637a46e6c0efbe57ce9f62ba5
Josh Melo <joshuamelo1126@gmail.com>
2021-08-15 21:39:30 -0400

    Updated font

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,14 @@ export default function Home() {
         <meta property="og:title" content="The Byte Size Blog" />
         <meta property="og:image" content="/images/pageImage.PNG" />
         <meta property="og:url" content="https://bytesizeblog.vercel.app/" />
+
         <meta property="og:description" content="Byte sized blog posts about tech." />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cousine:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <div className="mainContainer">
         <Header />
